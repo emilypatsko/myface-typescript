c9765d3c4b9700a07da2719787e2eba614265832
Emily Patsko <Emily.Patsko@softwire.com>
2021-02-05 11:24:31 +0000

    Added a button that when clicked changes the background colour of each post on the user profile page to a random colour

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,17 @@
+const { readBuilderProgram } = require("typescript");
+
 document.addEventListener('DOMContentLoaded', function() {
     // Add your JavaScript here.
 });
+
+function changeBackgroundColor() {
+    console.log("colour change");
+    const posts = document.getElementsByClassName("user_post");
+    for (let i = 0; i < posts.length; i++) {
+        posts[i].style.backgroundColor = generateRandomColor();
+    }
+}
+
+function generateRandomColor() {
+    return `rgb(${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)}, ${Math.floor(Math.random() * 256)})`;    
+}
